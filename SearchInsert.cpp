0c48f1b01f3094b536fcddbeb876fd7afe7390d8
PrathmeshGawade19 <prathmeshgawade1094@gmail.com>
2024-10-16 23:59:25 +0530

    Add Search Insert

---
+++ SearchInsert.cpp
@@ -0,0 +1,41 @@
+//Search Insert Problem
+#include<iostream>
+#include<algorithm>
+using namespace std;
+int main(){
+    int arr[]={1,3,5,6};
+    int arrsize=sizeof(arr)/sizeof(arr[0]);
+
+    int search=7;
+
+    sort(arr,arr+arrsize);
+
+    int s=0,e=arrsize-1;
+    int mid=s+(e-s)/2;
+    while(s<=e){   
+        if(arr[mid]==search){
+            cout<<"element "<<search<<" found at index : "<<mid<<endl;
+            break;
+        }
+        else if(arr[mid]<search){
+            s=mid+1;
+        }
+        else{
+            e=mid-1;
+        }
+        mid=s+(e-s)/2;
+    }
+
+    if (s > e) {
+        for(int i=0; i<arrsize; i++){
+        if(search<arr[i]){
+            cout<<"will put "<<search<<"at index :"<<i;
+            break;
+        } 
+        }
+    }
+
+    cout<<"will put "<<search<<"at index :"<<arrsize-1;
+
+    return 0;
+}
