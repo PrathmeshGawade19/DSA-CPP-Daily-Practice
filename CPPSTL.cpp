47cce255d658766b471db1d88b951cf40b2e5cb4
PrathmeshGawade19 <prathmeshgawade1094@gmail.com>
2024-10-20 20:55:46 +0530

    Add CPP STL Code
    Understood CPP STL and practiced the code

---
+++ CPPSTL.cpp
@@ -0,0 +1,69 @@
+#include<iostream>
+#include<vector> //dynamic array
+#include<list> //doubly linked list, random access not possible
+#include<deque> //double ended queue, random access possible
+//pair are part of cpp utility
+#include<stack> //LIFO
+#include<queue> //FIFO
+// priority_queue //largest value is given highest priority. Visualized using stack. Internally functions using BST. Sorts in descending order
+#include<map> //stores key-value where key is unique. Bydefault sorts data in ascending value according to key.
+
+using namespace std;
+
+int main(){
+    // pair<int,int> p={100,500};
+    // cout<<p.first<<endl;
+    // cout<<p.second<<endl;
+
+    // vector<pair<int,int>> vec={{1,2},{3,4},{4,5}};
+
+    // for(auto p:vec){
+    //     cout<<p.first<<" , "<<p.second<<endl;
+    // }
+    // cout<<endl;
+
+    // stack<int> mys;
+    // mys.push(10);
+    // mys.push(20);
+    // mys.push(30);
+    // mys.push(40);
+    // cout<<"mys size is : "<<mys.size()<<endl;
+
+    // // cout<<"top element of the stack is : "<<mys.top()<<endl;
+
+    // // mys.pop();
+
+    // // while(mys.empty()==0){
+    // //     cout<<mys.top()<<endl;
+    // //     mys.pop();
+    // // }
+
+    // stack<int> mys2;
+    // cout<<"mys2 size is : "<<mys2.size()<<endl;
+    // mys2.swap(mys);
+
+    // cout<<"mys size is : "<<mys.size()<<endl;
+    // cout<<"mys2 size is : "<<mys2.size()<<endl;
+
+    // priority_queue<int> pq;
+    // pq.push(1);
+    // pq.push(10);
+    // pq.push(200);
+    // pq.push(4);
+    // pq.push(50);
+
+    // cout<<"top element in priority queue is always the largest number so here it is : "<<pq.top()<<endl;
+
+    // while(!pq.empty()){
+    //     cout<<pq.top()<<endl;
+    //     pq.pop();
+    // }
+    // cout<<endl;
+
+
+    
+
+
+
+    return 0;
+}
