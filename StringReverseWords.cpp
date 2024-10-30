e5067b0a9397880df49c12a0781358ed6db6e4c4
PrathmeshGawade19 <prathmeshgawade1094@gmail.com>
2024-10-30 22:40:55 +0530

    Add string reverse words problem 2
    problem type 2 leetcode 151

--- StringReverseWords.cpp
+++ StringReverseWords.cpp
@@ -13,18 +13,59 @@ using namespace std;
 // Input: s = "Mr Ding"
 // Output: "rM gniD"
 
-int main(){ 
-    string s="Let's take LeetCode contest";
+// int main(){ 
+//     string s="Let's take LeetCode contest";
 
-    int b=0;
-    for(int i=0; i<=s.size(); i++){
-        if(s[i]==' ' || i==s.size()){
-            reverse(s.begin()+b,s.end()-(s.size()-i));
-            b=i+1;
+//     int b=0;
+//     for(int i=0; i<=s.size(); i++){
+//         if(s[i]==' ' || i==s.size()){
+//             reverse(s.begin()+b,s.end()-(s.size()-i));
+//             b=i+1;
+//         }
+//     }
+
+//     cout<<s<<endl;
+
+//     return 0;
+// }
+
+//Problem 2
+// Example 1:
+
+// Input: s = "the sky is blue"
+// Output: "blue is sky the"
+// Example 2:
+
+// Input: s = "  hello world  "
+// Output: "world hello"
+// Explanation: Your reversed string should not contain leading or trailing spaces.
+// Example 3:
+
+// Input: s = "a good   example"
+// Output: "example good a"
+// Explanation: You need to reduce multiple spaces between two words to a single space in the reversed string.
+
+int main(){ 
+    string s=" hello  world  ";
+    string ans="";
+
+    reverse(s.begin(),s.end());
+
+    for(int i=0; i<s.size(); i++){
+        string word="";
+        while(i<s.size() && s[i]!=' '){
+            word=word+s[i];
+            i++;
+        }
+
+        reverse(word.begin(),word.end());
+
+        if(word.length()>0){
+            ans=ans+" "+word;
         }
     }
 
-    cout<<s<<endl;
+    cout<<ans.substr(1)<<endl;
 
     return 0;
 }
