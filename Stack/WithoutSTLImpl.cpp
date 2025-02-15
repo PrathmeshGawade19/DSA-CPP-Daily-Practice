a63c59186a30dae1d8076b35b68a0ebfeb118173
Prathmesh Gawade <prathmeshgawade1094@gmail.com>
2025-02-15 18:14:25 +0530

    Update WithoutSTLImpl.cpp
    impl using array and LL

--- Stack/WithoutSTLImpl.cpp
+++ Stack/WithoutSTLImpl.cpp
@@ -1,9 +1,10 @@
 //implementation of stack without using stl 
 
 //array implementation of stack
+
 #include<iostream>
 using namespace std;
-
+/*
 class Stack{
     public:
     int *arr;
@@ -14,7 +15,7 @@ class Stack{
     Stack(int size){
         this->size = size;
         arr = new int[size];
-        this->top = -1;
+        this->top = -1; //because indexing in array starts from 0
     }
 
     //push operation
@@ -30,17 +31,19 @@ class Stack{
         }
     }
 
+    //pop operation
     void pop(){
         if(top == -1){
             cout<<"stack underflow occured!!!"<<endl;
         }
         else{
-            cout<<"popped element: " << arr[top] << endl;;
+            cout<<"popped element: " << arr[top] << endl;
             top--;
         }
     }
 
-    void topele(){
+    //top or peek element
+    void peek(){
         if(top >= 0){
             cout<<"top element of stack is : "<<arr[top]<<endl;
         }
@@ -49,6 +52,7 @@ class Stack{
         }  
     }
 
+    //stack empty or not
     void isempty(){
         if(top >= 0){
             cout<<"stack is non-empty"<<endl;
@@ -62,8 +66,6 @@ class Stack{
     ~Stack(){
         delete[] arr;
     }
-
-
 };
 
 int main(){
@@ -78,13 +80,96 @@ int main(){
     s.pop();
     s.pop();
     s.pop();
-    s.pop();
+    // s.pop();
     // s.pop();
 
-    s.topele();
+    s.peek();
 
     s.isempty();
 
+
+    return 0;
+}
+*/
+
+//linked list implementation of stack
+class Node{
+    public:
+    int data;
+    Node* next;
+
+    Node(int data){
+        this->data = data;
+        this->next = NULL;
+    }
+};
+
+class Stack{
+    public:
+    Node* top;
+
+    Stack(){
+        this->top = NULL;
+    }
+
+    //push
+    void push(int data){
+        Node* new_node = new Node(data);
+
+        if(!new_node){
+            cout<<"stack overflow occured!!!";
+        }
+
+        new_node->next = top;
+
+        top = new_node;
+    }
+
+    //pop
+    void pop(){
+        if(top==NULL){
+            cout<<"stack underflow occured!!!";
+        }
+
+        Node* temp = top;
+        top = top->next;
+        delete temp;
+    }
+
+    //peek
+    int peek() {
+        if (top!=NULL)
+            return top->data;
+        else {
+            cout << "\nStack is empty";
+            return INT32_MIN;
+        }
+    }
+
+    //empty
+    void isEmpty(){
+        if(top==NULL){
+            cout<<"stack is empty";
+        }
+        else{
+            cout<<"stack is not empty";
+        }
+    }
+};
+
+int main(){
+    Stack s;
+
+    s.push(10);
+    s.push(20);
+    s.push(30);
+
+    s.pop();
+
+    int ans = s.peek();
+    cout<<"peek element is "<<ans<<endl;
+
+    s.isEmpty();
 
     return 0;
 }
