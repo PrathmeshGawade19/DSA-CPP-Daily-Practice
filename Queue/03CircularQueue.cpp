3a2983767ef9da9de9e0efd6128335c4fbef238f
Prathmesh Gawade <prathmeshgawade1094@gmail.com>
2025-06-01 11:09:29 +0530

    Update 03CircularQueue.cpp

--- Queue/03CircularQueue.cpp
+++ Queue/03CircularQueue.cpp
@@ -1,4 +1,4 @@
-//circular queue implementation
+//implementation circular queue 
 #include<iostream>
 using namespace std;
 
@@ -9,6 +9,7 @@ class CircularQueue{
     int rear;
     int size;
 
+    //constructor
     CircularQueue(){
         size = 5;
         arr = new int[size];
@@ -16,47 +17,47 @@ class CircularQueue{
         rear = -1;
     }
 
+    //push operation in circular queue
     void enqueue(int data){
-        //case 1
+        //case 1 - queue if full 
         if((front == 0 && rear == size-1) || (rear == (front-1) % (size-1))){
             cout<<"queue is full"<<endl;
             return;
         }
-        //case 2
+        //case 2 - initial push
         else if(rear == -1){
             front = rear = 0;
             arr[rear] = data;
         }
-        //case 3
+        //case 3 - circular wrap around 
         else if(rear == size-1 && front != 0){
             rear = 0;
             arr[rear] = data;
         }
-        else{
+        else{ // normal case first increment rear and then push data
             rear++;
             arr[rear] = data;
         }
     }
 
-    int dequeue(){
-        if(front == -1){
+    void dequeue(){
+        if(front == -1){ // check empty or not before pop operation
             cout<<"queue is empty"<<endl;
-            return -1;
         }
-        else if(front == rear){
+        else if(front == rear){ //single element pop operation
             int ans = arr[front];
+            cout<<"the dequeued ele is : "<<ans<<endl;
             front = rear = -1;
-            return ans;
         }
-        else if(front == size-1){
+        else if(front == size-1){ //circular wrap around of front
             int ans = arr[front];
+            cout<<"the dequeued ele is : "<<ans<<endl;
             front = 0;
-            return ans;
         }
-        else{
-            int ans = arr[front];
+        else{ // normal case pop ele and then increment front
+            int ans = arr[front]; 
             front++;
-            return ans;
+            cout<<"the dequeued ele is : "<<ans<<endl;
         }
     }
 
@@ -65,12 +66,22 @@ class CircularQueue{
 
 int main(){
     CircularQueue q1;
-    // q1.enqueue(10);
-    // q1.enqueue(20);
-    // q1.enqueue(30);
-    // q1.enqueue(40);
-    // q1.enqueue(50);
+    q1.enqueue(10);
+    q1.enqueue(20);
+    q1.enqueue(30);
+    q1.enqueue(40);
+    q1.enqueue(50);
     
+    q1.dequeue();
+    q1.dequeue();
+    q1.dequeue();
+    q1.dequeue();
+
+    q1.enqueue(60);
+
+    q1.dequeue();
+    q1.dequeue();
+
     q1.dequeue();
     
 
